d15db5fd188213e5a05b40e20aa45c5d05e4d242
automater7795 <yashwanthquestin97@gmail.com>
2026-01-30 00:30:06 +0530

    yashii update

--- public/js/script.js
+++ public/js/script.js
@@ -14,6 +14,14 @@
           event.stopPropagation()
         }
 
+        Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
         form.classList.add('was-validated')
       }, false)
     })
